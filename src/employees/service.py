a5a187b7609c02f3722489719ec2138e14ee7fff
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-06-11 19:49:24 +0300

    change naming

--- src/employees/service.py
+++ src/employees/service.py
@@ -6,28 +6,28 @@ from src.database import get_async_session
 from sqlalchemy import insert, select, update, delete, func, desc
 
 from src.employees.exceptions import EmployeeAlreadyExists
-from src.employees.models import EmployeeOrm
-from src.employees.schemas import SEmployeeAdd, SEmployeeUpdate
-from src.tasks.models import TaskOrm, StatusEnum
+from src.employees.models import Employee
+from src.employees.schemas import EmployeeAddSchema, EmployeeUpdateSchema
+from src.tasks.models import Task, StatusEnum
 from src.tasks.service import get_important_tasks
 
 
 async def get_all_employees():
     async for session in get_async_session():
-        employees = await session.scalars(select(EmployeeOrm))
+        employees = await session.scalars(select(Employee))
         return employees
 
 
 async def get_employee_by_id(employee_id: int):
     async for session in get_async_session():
-        employee = await session.get(EmployeeOrm, employee_id)
+        employee = await session.get(Employee, employee_id)
         return employee
 
 
-async def add_employee(employee: SEmployeeAdd, user: User):
+async def add_employee(employee: EmployeeAddSchema, user: User):
     async for session in get_async_session():
         existing_employee = await session.scalars(
-            select(EmployeeOrm).where(EmployeeOrm.user_id == user.id)
+            select(Employee).where(Employee.user_id == user.id)
         )
         if existing_employee.first():
             detail = 'User already has a registered employee'
@@ -36,9 +36,9 @@ async def add_employee(employee: SEmployeeAdd, user: User):
         employee_data = employee.dict()
         employee_data['user_id'] = user.id
         try:
-            result = await session.scalars(insert(EmployeeOrm).
+            result = await session.scalars(insert(Employee).
                                            values(**employee_data).
-                                           returning(EmployeeOrm))
+                                           returning(Employee))
             created_employee = result.first()
             await session.commit()
             return created_employee
@@ -48,15 +48,15 @@ async def add_employee(employee: SEmployeeAdd, user: User):
             raise EmployeeAlreadyExists(detail)
 
 
-async def update_employee(employee, update_data: SEmployeeUpdate):
+async def update_employee(employee, update_data: EmployeeUpdateSchema):
     async for session in get_async_session():
         update_dict = update_data.dict(exclude_unset=True)
         try:
             result = await session.scalars(
-                update(EmployeeOrm)
-                .where(EmployeeOrm.id == employee.id)
+                update(Employee)
+                .where(Employee.id == employee.id)
                 .values(**update_dict)
-                .returning(EmployeeOrm)
+                .returning(Employee)
             )
             updated_employee = result.first()
             await session.commit()
@@ -69,22 +69,22 @@ async def update_employee(employee, update_data: SEmployeeUpdate):
 
 async def delete_employee(employee):
     async for session in get_async_session():
-        stmt = delete(EmployeeOrm).where(EmployeeOrm.id == employee.id)
+        stmt = delete(Employee).where(Employee.id == employee.id)
         await session.execute(stmt)
         await session.commit()
 
 
 async def get_busy_employees():
     async for session in get_async_session():
-        active_tasks = aliased(TaskOrm)
+        active_tasks = aliased(Task)
         query = (
-            select(EmployeeOrm, func.count(EmployeeOrm.tasks).label('active_task_count'))
+            select(Employee, func.count(Employee.tasks).label('active_task_count'))
             .outerjoin(active_tasks,
-                       (EmployeeOrm.id == active_tasks.employee_id) &
+                       (Employee.id == active_tasks.employee_id) &
                        (active_tasks.status == StatusEnum.in_process))
-            .group_by(EmployeeOrm.id)
+            .group_by(Employee.id)
             .order_by(desc('active_task_count'))
-            .options(joinedload(EmployeeOrm.tasks))
+            .options(joinedload(Employee.tasks))
         )
         result = await session.execute(query)
         return result.unique().scalars()
@@ -92,13 +92,13 @@ async def get_busy_employees():
 
 async def get_least_busy_employee():
     async for session in get_async_session():
-        active_tasks = aliased(TaskOrm)
+        active_tasks = aliased(Task)
         query = (
-            select(EmployeeOrm, func.count(active_tasks.id).label('active_task_count'))
+            select(Employee, func.count(active_tasks.id).label('active_task_count'))
             .outerjoin(active_tasks,
-                       (EmployeeOrm.id == active_tasks.employee_id) &
+                       (Employee.id == active_tasks.employee_id) &
                        (active_tasks.status == StatusEnum.in_process))
-            .group_by(EmployeeOrm.id)
+            .group_by(Employee.id)
             .order_by('active_task_count')
             .limit(1)
         )
@@ -109,9 +109,9 @@ async def get_least_busy_employee():
 async def get_employee_for_parent_task(parent_task_id: int):
     async for session in get_async_session():
         query = (
-            select(EmployeeOrm)
-            .join(TaskOrm, TaskOrm.employee_id == EmployeeOrm.id)
-            .where(TaskOrm.id == parent_task_id)
+            select(Employee)
+            .join(Task, Task.employee_id == Employee.id)
+            .where(Task.id == parent_task_id)
         )
         result = await session.execute(query)
         return result.scalar()
@@ -127,14 +127,14 @@ async def get_important_tasks_and_employees():
             parent_task_employee = await get_employee_for_parent_task(task.parent_task_id)
             if parent_task_employee:
                 parent_task_employee_active_tasks_count = await session.execute(
-                    select(func.count(TaskOrm.id))
-                    .where(TaskOrm.employee_id == parent_task_employee.id, TaskOrm.status == StatusEnum.in_process)
+                    select(func.count(Task.id))
+                    .where(Task.employee_id == parent_task_employee.id, Task.status == StatusEnum.in_process)
                 )
                 parent_task_employee_active_tasks_count = parent_task_employee_active_tasks_count.scalar()
 
                 least_busy_employee_active_tasks_count = await session.execute(
-                    select(func.count(TaskOrm.id))
-                    .where(TaskOrm.employee_id == least_busy_employee.id, TaskOrm.status == StatusEnum.in_process)
+                    select(func.count(Task.id))
+                    .where(Task.employee_id == least_busy_employee.id, Task.status == StatusEnum.in_process)
                 )
                 least_busy_employee_active_tasks_count = least_busy_employee_active_tasks_count.scalar()
 
--- src/tasks/service.py
+++ src/tasks/service.py
@@ -2,20 +2,20 @@ from sqlalchemy import insert, select, update, delete
 from sqlalchemy.orm import aliased
 
 from src.database import get_async_session
-from src.tasks.models import TaskOrm, StatusEnum
-from src.tasks.schemas import STaskUpdate, STaskAdd
+from src.tasks.models import Task, StatusEnum
+from src.tasks.schemas import TaskUpdateSchema, TaskAddSchema
 
 
 async def get_all_tasks():
     async for session in get_async_session():
-        tasks = await session.scalars(select(TaskOrm))
+        tasks = await session.scalars(select(Task))
         return tasks
 
 
 async def get_important_tasks():
     async for session in get_async_session():
-        important_tasks = aliased(TaskOrm)
-        dependent_tasks = aliased(TaskOrm)
+        important_tasks = aliased(Task)
+        dependent_tasks = aliased(Task)
 
         query = (
             select(important_tasks)
@@ -29,26 +29,26 @@ async def get_important_tasks():
 
 async def get_task_by_id(task_id: int):
     async for session in get_async_session():
-        task = await session.get(TaskOrm, task_id)
+        task = await session.get(Task, task_id)
         return task
 
 
-async def add_task(task: STaskAdd):
+async def add_task(task: TaskAddSchema):
     async for session in get_async_session():
-        result = await session.scalars(insert(TaskOrm).values(**task.dict()).returning(TaskOrm))
+        result = await session.scalars(insert(Task).values(**task.dict()).returning(Task))
         created_task = result.first()
         await session.commit()
         return created_task
 
 
-async def update_task(task, update_data: STaskUpdate):
+async def update_task(task, update_data: TaskUpdateSchema):
     async for session in get_async_session():
         update_dict = update_data.dict(exclude_unset=True)
         result = await session.scalars(
-            update(TaskOrm)
-            .where(TaskOrm.id == task.id)
+            update(Task)
+            .where(Task.id == task.id)
             .values(**update_dict)
-            .returning(TaskOrm)
+            .returning(Task)
         )
         updated_task = result.first()
         await session.commit()
@@ -57,6 +57,6 @@ async def update_task(task, update_data: STaskUpdate):
 
 async def delete_task(task):
     async for session in get_async_session():
-        stmt = delete(TaskOrm).where(TaskOrm.id == task.id)
+        stmt = delete(Task).where(Task.id == task.id)
         await session.execute(stmt)
         await session.commit()
